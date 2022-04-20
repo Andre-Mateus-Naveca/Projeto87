c3fc49c65964273baf459f27d9c78d64f7b75520
Andre-Mateus-Naveca <95716954+Andre-Mateus-Naveca@users.noreply.github.com>
2022-04-20 14:44:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+
+// Create canvas variable
+var canvas = new fabric.Canvas('myCanvas');
+ blockY=1;
+ blockX=1;
+
+blockImageWidth = 350;
+blockImageHeight = 430;
+
+var blockImageObject= "";
+
+function newImage(getImage)
+{
+	// para enviar imagens
+	fabric.Image.fromURL(getImage, function(Img) {
+		blockImageObject = Img;
+
+		blockImageObject.scaleToWidth(blockImageWidth);
+		blockImageObject.scaleToHeight(blockImageHeight);
+		blockImageObject.set({
+			top:blockY,
+			left:blockX
+		});
+		canvas.add(blockImageObject);
+	});
+}
+
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '69') // adicione os códigos adequados às teclas
+	{
+		// enviar ranger escarlate
+		newImage('rr1.png');
+		console.log("e")
+	}
+	if(keyPressed == '71')
+	{
+		blockX = 200;
+		// enviar ranger verde
+		newImage('gr.png');
+		console.log("g")
+	}
+	
+	if(keyPressed == '65')
+	{
+		blockX =350;
+		// enviar ranger amarelo
+		newImage('yr.png');
+		console.log("a")
+	}
+	if(keyPressed == '82')
+	{
+		blockX = 600;
+		// enviar ranger rosa
+		newImage('pr.png');
+		console.log("r")
+	}
+	if(keyPressed == '73')
+	{
+		blockX = 700;
+	// enviar ranger índigo
+	newImage('br.png')
+	console.log("i")
+	}
+	
+}
+
